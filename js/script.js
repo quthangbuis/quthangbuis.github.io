e89b9da5e32e507a64b143d5e9f8c5783e0c16f6
quthangbuis <90095299+quthangbuis@users.noreply.github.com>
2021-11-28 10:46:05 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
 const   $ = (x) => document.querySelector(x)
 const   $$ = (x) => document.querySelectorAll(x)
-
 //  var
-const  containerNavSelectorHeading = $('.container__nav-selector--heading') 
-const  containerNavAbout = $('.container__nav-about') 
-const  containerList = $('.container__list')
+const   containerNavSelectorHeading = $('.container__nav-selector--heading') 
+const   containerNavAbout = $('.container__nav-about') 
+const   containerList = $('.container__list')
+const   HeaderSearchInput = $('.header-search__input')
+const   HeaderResultList = $('.header-result__list')
+const   DeleteChar = $('.header-search__btn--del')
+
+let     SubChemiCalContainer
+let     ListAudioContainer 
+let     ListShowContainer 
+let     ListPlayContainer 
+
+let     SubChemiCalHeader
+let     ListAudioHeader
+let     ListShowHeader
+let     ListPlayHeader
 
 
 // Code
 
 const   app = {  
-    current: 4,
+    current: 1,
     Types : [
+        {
+            name: 'Nguyên tố',
+            array: NguyenTo,
+            about: ' là một chất hóa học tinh khiết, bao gồm một kiểu nguyên tử, được phân biệt bởi số hiệu nguyên tử, là số lượng proton có trong mỗi hạt nhân.'
+        },
         {
             name: 'Axit',
             array: Axit,
@@ -38,11 +55,6 @@ const   app = {
             about: ' là những từ ngữ biểu thị khái niệm khoa học công nghệ, chủ yếu để dùng trong các văn bản khoa học công nghệ.'
         },
         {
-            name: 'Nguyên tố',
-            array: NguyenTo,
-            about: ' là một chất hóa học tinh khiết, bao gồm một kiểu nguyên tử, được phân biệt bởi số hiệu nguyên tử, là số lượng proton có trong mỗi hạt nhân.'
-        },
-        {
             name: 'Khác',
             array: Khac,
             about: ''
@@ -50,13 +62,9 @@ const   app = {
     ],
     renderMap: function ( current ) {
         let Obj = this.Types[current]
-
         containerNavSelectorHeading.innerHTML = `<h1>${Obj.name}</h1>`
-        containerNavAbout.innerHTML = `<b>${Obj.name} là gì?</b>
-        <p><b>${Obj.name}</b>${Obj.about}</p>`
-
+        containerNavAbout.innerHTML = (current != 6) ? `<b>${Obj.name} là gì?</b><p><b>${Obj.name}</b>${Obj.about}</p>` : ''
         let Arr = Obj.array
-
         let html = Obj.array.map(Item => {
             return `
                 <div class="chemical">
@@ -68,7 +76,7 @@ const   app = {
                             <ion-icon name="star-sharp"></ion-icon>
                         </div>
                     </div>
-                    <div class="chemical--secondary chemical__item">
+                    <div class="chemical--secondary chemical__item" style="display: none;">
                         <div class="chemical--secondary-heading chemical__item-heading">
                             <div class="chemical--secondary-heading--first chemical__item-heading--first"><b>${Item.name}</b></div>
                             <div class="chemical--secondary-heading--last chemical__item-heading--child"><b>${Item.transcribe}</b></div>
@@ -77,10 +85,25 @@ const   app = {
                             <ion-icon name="volume-high-sharp"></ion-icon>
                         </div>
                     </div>
+                    <audio src="${Item.sound}" class = "container__audio"></audio>
                 </div>
             `
         })
         containerList.innerHTML = html.join('')
+        ListAudioContainer = $$('.container__audio')
+        ListShowContainer = $$('.container .chemical--primary-option')
+        ListPlayContainer = $$('.container .chemical--secondary-option')
+        SubChemiCalContainer = $$('.container .chemical--secondary')
+
+
+        for(let i = 0; i < ListShowContainer.length; ++i) {
+            ListShowContainer[i].onclick = () => {
+                SubChemiCalContainer[i].style.display = ( SubChemiCalContainer[i].style.display == 'none') ? 'flex' : 'none'
+            }
+            ListPlayContainer[i].onclick = () => {
+                ListAudioContainer[i].play()
+            }
+        }
     },
     handleChangeMap: function( ) {
         let Left = $('.container__nav-selector__btn1')
@@ -94,9 +117,91 @@ const   app = {
             this.renderMap(this.current)
         }
     },
+    search: function () {
+        HeaderSearchInput.onkeyup = (e) => {
+            let UserData = e.target.value
+            if( UserData ) {
+                const   check = ( obj ) => {
+                    const SubCheck = (s, st) => {
+                        s = s.toLowerCase()
+                        st = st.toLowerCase()
+                        let i = 0
+                        for(let j = 0; j < st.length; ++j) i = (s[i] == st[j]) ? i + 1 : i
+                        return i == s.length
+                    }
+                    return (SubCheck(UserData,obj.symbol))
+                }
+                let Result = Items.filter(check)
+                Result.sort((a,b) => {
+                    let     str = UserData[0].toLowerCase()
+                    let     x = a.symbol.indexOf(str)
+                    let     y = b.symbol.indexOf(str)
+                    return x - y
+                })
+                while( Result.length > 10) Result.pop()
+                let htmls = Result.map(Item => {
+                    return `
+                    <div class="chemical">
+                        <div class="icon">
+                            <ion-icon name="search-outline"></ion-icon>
+                        </div>
+                        <div class="content">
+                            <div class="chemical--primary chemical__item">
+                                <div class="chemical--primary-heading chemical__item-heading">
+                                    <div class="chemical--primary-heading--first chemical__item-heading--first"><p>${Item.symbol}</p></div>
+                                    <div class="chemical--primary-heading--last chemical__item-heading--last"><p>${Item.type}</p></div>
+                                </div>
+                                <div class="chemical--primary-option chemical__item-option">
+                                    <ion-icon name="star-sharp"></ion-icon>
+                                </div>
+                            </div>
+                            <div class="chemical--secondary chemical__item" style="display: none;">
+                                <div class="chemical--secondary-heading chemical__item-heading">
+                                    <div class="chemical--secondary-heading--first chemical__item-heading--first"><p>${Item.name}</p></div>
+                                    <div class="chemical--secondary-heading--last chemical__item-heading--child"><p>${Item.transcribe}</p></div>
+                            </div>
+                        <div class="chemical--secondary-option chemical__item-option">
+                            <ion-icon name="volume-high-sharp"></ion-icon>
+                        </div>
+                    </div>
+                            <audio src="${Item.sound}" class = "header__audio"></audio>
+                        </div>
+                    </div>
+                    `
+                })
+                HeaderResultList.innerHTML = htmls.join('')
+                DeleteChar.style.display = 'flex'
+                DeleteChar.onclick = () => {
+                    HeaderSearchInput.value = ''
+                    HeaderResultList.innerHTML = ''
+                    DeleteChar.style.display = 'none'
+                }
+
+                SubChemiCalHeader = $$('.header .chemical--secondary')
+                ListAudioHeader  = $$('.header__audio')
+                ListShowHeader  = $$('.header .chemical--primary-option ')
+                ListPlayHeader  = $$('.header .chemical--secondary-option')
+                for(let i = 0; i < ListShowHeader.length; ++i) {
+                    ListShowHeader[i].onclick = () => {
+                        SubChemiCalHeader[i].style.display = ( SubChemiCalHeader[i].style.display == 'none') ? 'flex' : 'none'
+                    }
+                    ListPlayHeader[i].onclick = () => {
+                        ListAudioHeader[i].play()
+                    }
+                }
+
+                
+
+            } else {
+                HeaderResultList.innerHTML = ''
+                DeleteChar.style.display = 'none'
+            }
+        }
+    },
     start: function () {
         this.renderMap(this.current)
         this.handleChangeMap()
+        this.search()
     }
 }
 
